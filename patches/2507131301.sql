077d8245ce2e7aef00e8a53af02a89dec12fd3a6
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-13 17:11:50 +0300

    add user function

--- patches/2507131301.sql
+++ patches/2507131301.sql
@@ -14,15 +14,14 @@ GRANT USAGE ON SCHEMA access TO tpss;
 
 CREATE TABLE IF NOT EXISTS access.users
 (
-    id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    created timestamp with time zone NOT NULL DEFAULT now(),
+    id       uuid                     NOT NULL DEFAULT uuid_generate_v4(),
+    created  timestamp with time zone NOT NULL DEFAULT now(),
     username text COLLATE pg_catalog."default",
     password text COLLATE pg_catalog."default",
-    name text COLLATE pg_catalog."default",
+    name     text COLLATE pg_catalog."default",
     CONSTRAINT users_pkey PRIMARY KEY (id),
     CONSTRAINT users_username_uniq UNIQUE (username)
 )
-
     TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS access.users
@@ -30,3 +29,52 @@ ALTER TABLE IF EXISTS access.users
 
 COMMENT ON TABLE access.users
     IS 'Пользователи';
+
+-- Extension: pgcrypto
+
+-- DROP EXTENSION pgcrypto;
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- FUNCTION: access.add_user(text, text, text)
+
+-- DROP FUNCTION IF EXISTS access.add_user(text, text, text);
+
+CREATE OR REPLACE FUNCTION access.add_user(
+    a_username text,
+    a_password text,
+    a_name text)
+    RETURNS uuid
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS
+$BODY$
+DECLARE
+    v_id uuid DEFAULT NULL;
+BEGIN
+
+    INSERT INTO access.users (username,
+                              password,
+                              name)
+    VALUES (a_username,
+            crypt(a_password, gen_salt('bf')),
+            a_name)
+    RETURNING id INTO v_id;
+
+    RETURN v_id;
+
+END;
+$BODY$;
+
+ALTER FUNCTION access.add_user(text, text, text)
+    OWNER TO postgres;
+
+COMMENT ON FUNCTION access.add_user(text, text, text)
+    IS 'Добавляет пользователя';
+
+SELECT access.add_user(
+               'test',
+               '123',
+               'Тестовый Тест Тестович'
+       );
