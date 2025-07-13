94af1d82b18e922b65aa8c2e11d112af636e86ee
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-13 14:03:11 +0300

    added users table

--- patches/2507131301.sql
+++ patches/2507131301.sql
@@ -1,2 +1,32 @@
 COMMENT ON DATABASE tpss
     IS 'v25.07.13.13:01';
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE SCHEMA access
+    AUTHORIZATION postgres;
+
+GRANT USAGE ON SCHEMA access TO tpss;
+
+-- Table: access.users
+
+-- DROP TABLE IF EXISTS access.users;
+
+CREATE TABLE IF NOT EXISTS access.users
+(
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    created timestamp with time zone NOT NULL DEFAULT now(),
+    username text COLLATE pg_catalog."default",
+    password text COLLATE pg_catalog."default",
+    name text COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_username_uniq UNIQUE (username)
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS access.users
+    OWNER to postgres;
+
+COMMENT ON TABLE access.users
+    IS 'Пользователи';
